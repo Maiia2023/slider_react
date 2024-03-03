d80361806644973a969cf57ba08957d2aab43162
Maya B <newmay4@gmail.com>
2024-03-03 09:52:39 +0300

    Added App

---
+++ src/app/App.js
@@ -0,0 +1,15 @@
+import './styles/index.scss';
+// import { usePhotos } from 'shared/hooks';
+// import { Slider } from 'features';
+
+export const App = () => {
+  // const photoStrate = usePhotos();
+  // photoState.getPhotos();
+  return (
+    <div className={'app'}>
+      React Slider
+      { /* <Slider slides {photoState.photos}/> */}
+      { /* <Slider slides {[]}/> */}
+    </div>
+  );
+};
